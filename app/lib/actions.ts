b4b3cf1016246688140fb9b2fac00f2dcd02949f
7FOX7 <kheeugene@gmail.com>
2024-10-17 22:05:10 -0400

    Added validation check logic for 'create invoice' form

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -4,26 +4,55 @@ import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 import { z } from "zod";
 
+
+// Custom type
+export type State = {
+   message?: string | null, 
+   errors?: {
+      customerId?: string[], 
+      amount?: string[], 
+      status?: string[]
+   }
+}
+
 const FormData = z.object({
    id: z.string(), 
-   customerId: z.string(), 
-   amount: z.coerce.number(), 
-   status: z.enum(['pending', 'paid']), 
+   customerId: z.string({
+      invalid_type_error: "Please Select a customer"
+   }), 
+   amount: z.coerce
+      .number()
+      .gt(0, {message: 'Please Enter a value greater than $0'}), 
+   status: z.enum(['pending', 'paid'], {
+      invalid_type_error: "Please Select a status type"
+   }), 
    date: z.string()
 })
-
 
 const CustomerData = FormData.omit({id: true, date: true})
 
 const UpdateInvoice = FormData.omit({id: true, date: true})
 
-export async function createInvoice(formData: FormData) {  
-   const {customerId, amount, status} = CustomerData.parse({
+// prevState is containing all the errors we have got when submitting the form
+export async function createInvoice(prevState: State, formData: FormData) {  
+   const validatedFields = CustomerData.safeParse({      // safeParse will return us true/false of whether convertion was successful
       customerId: formData.get('customerId'), 
       amount: formData.get('amount'), 
       status: formData.get('status'), 
    })
 
+   // If form validation fails, return errors early. Otherwise, continue. They will be returned 
+   // to the 'state' object
+   if(!validatedFields.success) {
+      console.log('-- you are about to fail, (((( --')
+      return {
+         message: 'Missing Fields. Failed to create an invoice.', 
+         errors: validatedFields.error.flatten().fieldErrors
+      }
+   }
+
+   console.log('-- Looks like you got everything right --')
+   const {customerId, amount, status} = validatedFields.data; 
    const amountInCents = amount * 100
    const date = new Date().toISOString().split('T')[0]
 
