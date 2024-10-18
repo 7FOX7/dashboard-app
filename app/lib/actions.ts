6924a5189093f6a43557b623a406ec243f4179b1
7FOX7 <kheeugene@gmail.com>
2024-10-17 23:19:01 -0400

    Added validation check logic for 'edit invoice' form

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -12,7 +12,7 @@ export type State = {
       customerId?: string[], 
       amount?: string[], 
       status?: string[]
-   }
+   }, 
 }
 
 const FormData = z.object({
@@ -44,14 +44,14 @@ export async function createInvoice(prevState: State, formData: FormData) {
    // If form validation fails, return errors early. Otherwise, continue. They will be returned 
    // to the 'state' object
    if(!validatedFields.success) {
-      console.log('-- you are about to fail, (((( --')
+      console.log('-- validation failed (error will be thrown) --')
       return {
          message: 'Missing Fields. Failed to create an invoice.', 
          errors: validatedFields.error.flatten().fieldErrors
       }
    }
 
-   console.log('-- Looks like you got everything right --')
+   console.log('-- validation succeded (error will NOT be thrown) --')
    const {customerId, amount, status} = validatedFields.data; 
    const amountInCents = amount * 100
    const date = new Date().toISOString().split('T')[0]
@@ -71,15 +71,27 @@ export async function createInvoice(prevState: State, formData: FormData) {
    redirect('/dashboard/invoices')  // this will send the user to the specified path
 } 
 
-export async function updateInvoice(id: string, formData: FormData) {
-   const { customerId, amount, status } = UpdateInvoice.parse({
+export async function updateInvoice(id: string, prevState: State, formData: FormData) {
+   const validatedFields = UpdateInvoice.safeParse({
       customerId: formData.get('customerId'),
       amount: formData.get('amount'),
       status: formData.get('status'),
    });
 
-   const amountInCents = amount * 100;
+   if(!validatedFields.success) {
+      console.log('-- validation failed (error will be thrown) --')
+      return {
+         message: 'Missing Fields. Failed to create an invoice.', 
+         errors: validatedFields.error.flatten().fieldErrors, 
+         additional: 'hello'
+      }
+   }
 
+   console.log('-- validation succeded (error will NOT be thrown) --')
+
+   const {customerId, amount, status} = validatedFields.data;
+
+   const amountInCents = amount * 100; 
    try {
       await sql`
       UPDATE invoices
